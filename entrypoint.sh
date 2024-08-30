ed5f4f29325746ce05889c0eb956495bade27af5
Mikolaj Sobolewksi <mikolaj@meganuke.ru>
2024-08-30 13:25:27 +0300

    Add some debug information

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ trap stop SIGTERM SIGINT SIGQUIT
 
 if [ ! -c /dev/net/tun ]; then
     sudo mkdir -p /dev/net
+    ls -la /dev
     sudo mknod /dev/net/tun c 10 200
+    ls -la /dev/net
     sudo chmod 600 /dev/net/tun
 fi
 wg-quick up /etc/wireguard/wg0.conf
